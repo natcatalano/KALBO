edfe3304d9d881bd082c37ba7f36aa7d20edf8c1
Bruno Percara <bruno.percara@gmail.com>
2024-02-06 17:13:45 -0300

    Kalbo
    carrusel premium

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const buttonsWrapper = document.querySelector(".listas-packs");
-const slides = document.querySelector(".carrousel");
+const slides = document.querySelector(".inner");
 
 buttonsWrapper.addEventListener("click", e => {
   if (e.target.nodeName === "BUTTON") {
@@ -7,16 +7,16 @@ buttonsWrapper.addEventListener("click", e => {
       item.classList.remove("active")
     );
     if (e.target.classList.contains("first")) {
-      slides.style.transform = "translateX(-0%)";
+      slides.style.transform = "translateX(33%)";
       e.target.classList.add("active");
     } else if (e.target.classList.contains("second")) {
-      slides.style.transform = "translateX(-33.33333333333333%)";
+      slides.style.transform = "translateX(-0%)";
       e.target.classList.add("active");
     } else if (e.target.classList.contains("third")){
-      slides.style.transform = 'translatex(-66.6666666667%)';
+      slides.style.transform = 'translatex(-25%)';
       e.target.classList.add('active');
     } else if (e.target.classList.contains("four")){
-        slides.style.transform = 'translatex(-99.9999999990%)';
+        slides.style.transform = 'translatex(-35%)';
         e.target.classList.add('active');
     }
   }
