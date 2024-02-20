8fba33e420b6a1c74783b8314bbfee0ba045453e
Bruno Percara <bruno.percara@gmail.com>
2024-02-20 20:20:49 -0300

    Kalbo

--- script.js
+++ script.js
@@ -126,3 +126,12 @@ document.addEventListener('DOMContentLoaded', function () {
 	});
 
 });
+
+//limpia los campos de datos despues de enviarlos 
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+	// Limpia los campos de entrada después de enviar el formulario
+	document.getElementById("name").value = "";
+	document.getElementById("email").value = "";
+	document.getElementById("tel").value = "";
+	    
+});
